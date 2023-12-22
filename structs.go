80679c108701260d0695cc2a9584bab8d9d99d00
iamdanielyin <iamdanielyin@gmail.com>
2023-12-22 18:48:49 +0800

    fix: 修复permissions反序列化失败的bug

--- structs.go
+++ structs.go
@@ -1241,7 +1241,7 @@ type Role struct {
 	// The permissions of the role on the guild (doesn't include channel overrides).
 	// This is a combination of bit masks; the presence of a certain permission can
 	// be checked by performing a bitwise AND between this int and the permission.
-	Permissions int64 `json:"permissions,string"`
+	Permissions int64 `json:"permissions"`
 }
 
 // Mention returns a string which mentions the role
